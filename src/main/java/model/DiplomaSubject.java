db690bc17bd7a890f9b42f38dc218e9bd3f461b2
marios4371 <cs04371@uoi.gr>
2023-04-12 16:32:36 +0300

    Commit Model classes

--- src/main/java/model/DiplomaSubject.java
+++ src/main/java/model/DiplomaSubject.java
@@ -1,4 +1,16 @@
 package model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString(callSuper = true)
 public class DiplomaSubject {
+
+    private String title;
+    private String objective;
 }
